15e0b9cbc54353d4d4ba0f3a4a43831ad05c5fce
Hani <hani.hashemi@mobiliva.com>
2019-09-23 22:01:26 +0300

    Change Home to Timer and Dashboard to History

--- app/src/main/java/hanihashemi/github/io/ui/timer/TimerFragment.kt
+++ app/src/main/java/hanihashemi/github/io/ui/timer/TimerFragment.kt
@@ -1,4 +1,4 @@
-package hanihashemi.github.io.ui.dashboard
+package hanihashemi.github.io.ui.timer
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import hanihashemi.github.io.R
 
-class DashboardFragment : Fragment() {
+class TimerFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: DashboardViewModel
+    private lateinit var timerViewModel: TimerViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
-            ViewModelProviders.of(this).get(DashboardViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        dashboardViewModel.text.observe(this, Observer {
+        timerViewModel =
+            ViewModelProviders.of(this).get(TimerViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_timer, container, false)
+        val textView: TextView = root.findViewById(R.id.text_home)
+        timerViewModel.text.observe(this, Observer {
             textView.text = it
         })
         return root
--- app/src/main/java/hanihashemi/github/io/ui/timer/TimerViewModel.kt
+++ app/src/main/java/hanihashemi/github/io/ui/timer/TimerViewModel.kt
@@ -1,13 +1,13 @@
-package hanihashemi.github.io.ui.dashboard
+package hanihashemi.github.io.ui.timer
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class TimerViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is home Fragment"
     }
     val text: LiveData<String> = _text
 }
